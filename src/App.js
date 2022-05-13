38bc1650cd5ac6ec4824bfa78a2c9c85390f8eea
Nam Min Ki <nam960216@gmail.com>
2022-05-13 17:29:09 +0900

    all

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setVaule] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setVaule((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;

9fe08e39f29089bf1af464de4c72f19ba3201daa
Nam Min Ki <nam960216@gmail.com>
2022-05-14 14:50:13 +0900

    js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setVaule] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setVaule((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
+  return <h1>Hello</h1>;
+}
 
-  console.log("I run all the time");
-  useEffect(() => {
-    console.log("CALL THE API....");
-  }, []);
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

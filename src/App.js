ea3af25747aeb50bca091d202623f49aefeef897
Nam Min Ki <nam960216@gmail.com>
2022-05-14 15:42:45 +0900

    js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 
 function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
   return <h1>Hello</h1>;
 }
 

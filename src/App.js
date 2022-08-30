65bd016351607210c45ff0f0bdd5c330cd4df73d
Nam Min Ki <nam960216@gmail.com>
2022-08-30 23:52:54 +0900

    220830

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ function App() {
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("I run only once.");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword or counter changes.");
+  }, [keyword, counter]);
   return (
     <div>
       <input

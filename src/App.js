2263fbc62befabf3c71e48ef2c010fb8f8d1eed8
Nam Min Ki <nam960216@gmail.com>
2022-09-01 17:43:18 +0900

    220901

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  return <div></div>;
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
 }
 
 export default App;
